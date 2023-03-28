046f061e938c6f30ee4bba8337baa385f870b66a
Martin Andersson <martin.andersson@me.com>
2023-03-28 10:16:43 +0200

    fixed mobilview

--- .next/static/webpack/webpack.81bf958dfcd9fa37.hot-update.js
+++ .next/static/webpack/webpack.81bf958dfcd9fa37.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b414185428a938f8"; }
+/******/ 	__webpack_require__.h = function() { return "31729b3e08e93642"; }
 /******/ }();
 /******/ 
 /******/ }
